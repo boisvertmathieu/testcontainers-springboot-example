7dfea0333e9fd7ec26a564f9809b01bc445f2b6e
Mathieu Boisvert <boisvert.my@gmail.com>
2023-02-11 15:43:56 -0500

    Added tests and a readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Don't forget to start the docker container or else the Spring Boot application won't boot up :
+
+`docker compose up -d`
--- src/test/java/com/example/demo/domain/UserServiceTest.java
+++ src/test/java/com/example/demo/domain/UserServiceTest.java
@@ -1,10 +1,17 @@
 package com.example.demo.domain;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.entity.UserEntity;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -13,8 +20,10 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 @Testcontainers
@@ -44,11 +53,51 @@ class UserServiceTest {
     @Autowired
     UserService userService;
 
+    @SpyBean
+    UserRepository userRepository;
+
+    @Captor
+    ArgumentCaptor<UserEntity> userEntityCaptor;
+
     @Test
-    void getAllUsers_doitRetournerTousLesUsers() {
+    void getAllUsers_shouldNotBeEmpty() {
         List<User> users = userService.getAllUsers();
 
         assertThat(users).isNotEmpty();
+    }
+
+    @Test
+    void getUserById_shouldReturnUser() throws ResourceNotFoundException {
+        User user = userService.findById(UUID.fromString("e0a084b3-c9f8-4d22-b988-29561a4961c1"));
+
+        assertThat(user).isNotNull();
+        assertThat(user.getFirstName()).isEqualTo("Skip");
+        assertThat(user.getLastName()).isEqualTo("Hovert");
+        assertThat(user.getEmail()).isEqualTo("chovert2@economist.com");
+    }
+
+    @Test
+    void createUser_shouldCreateUser() {
+        User user = getValidUser();
+
+        userService.createUser(user);
+
+        verify(userRepository).save(userEntityCaptor.capture());
+
+        UserEntity userEntity = userEntityCaptor.getValue();
+        assertThat(userEntity).isNotNull();
+        assertThat(userEntity).usingRecursiveComparison()
+                .ignoringFields("id", "createdAt", "createdBy", "updatedAt", "updatedBy")
+                .isEqualTo(user);
+
+    }
+
+    private User getValidUser() {
+        User user = new User();
+        user.setFirstName("Firstname");
+        user.setLastName("Lastname");
+        user.setEmail("email@address.com");
+        return user;
     }
 
 }
