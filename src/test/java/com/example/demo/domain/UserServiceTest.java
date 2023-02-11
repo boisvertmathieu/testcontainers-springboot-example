37783a09652ec5e5334c1b59cb0d8e6e33fc9374
Mathieu Boisvert <boisvert.my@gmail.com>
2023-02-11 15:31:28 -0500

    Updated the gitignore to remove the build and .gradle folder, added the test profile annotation to the test class

--- src/test/java/com/example/demo/domain/UserServiceTest.java
+++ src/test/java/com/example/demo/domain/UserServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MSSQLServerContainer;
@@ -18,6 +19,7 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class UserServiceTest {
 
     @Container
